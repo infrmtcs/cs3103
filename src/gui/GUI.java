5b8000556c34db3a4e803a92f2630acf655cc504
Le Cong Thang <lecongthang.1102@gmail.com>
2015-11-02 13:04:33 +0800

    no message

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.EventQueue;
@@ -22,6 +23,7 @@ import java.awt.BorderLayout;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.JApplet;
 import javax.swing.JButton;
 
 import java.awt.event.ActionListener;
@@ -36,18 +38,20 @@ import javax.swing.border.EmptyBorder;
 
 import controller.Controller;
 
-public class GUI {
+public class GUI extends JFrame{
 	
+	private final static String CRAWLER_NAME = "PREPOCHECKER";
 	private final static String EMPTY_SEARCH_STRING = "Please input a key word";
-
+	private final static String EMPTY_STRING = "";
+	
 	private JFrame frame;
 	private JTextField textField;
 	private JButton btnSearch;
 	private JPanel panel;
 	private JPanel prepocheckerLayout;
 	private JPanel panel_1;
-	private JLabel warning;
-	private JPanel panel_2;
+	private JLabel result;
+	private JPanel resultPanel;
 
 	/**
 	 * Launch the application.
@@ -76,7 +80,7 @@ public class GUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame("PrepoChecker");
+		frame = new JFrame(CRAWLER_NAME);
 		frame.setBounds(100, 100, 501, 330);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
@@ -90,7 +94,7 @@ public class GUI {
 		prepocheckerLayout.add(panel);
 		panel.setBorder(new EmptyBorder(10, 10, 15, 10));
 		
-		JLabel lblPrepochecker = new JLabel("PREPOCHECKER");
+		JLabel lblPrepochecker = new JLabel(CRAWLER_NAME);
 		panel.add(lblPrepochecker);
 		lblPrepochecker.setFont(new Font(".VnClarendonH", Font.PLAIN, 20));
 		lblPrepochecker.setForeground(Color.BLUE);
@@ -105,24 +109,27 @@ public class GUI {
 		btnSearch = new JButton("Prepochecker Search");
 		panel_1.add(btnSearch);
 		
-		panel_2 = new JPanel();
-		prepocheckerLayout.add(panel_2);
+		resultPanel = new JPanel();
+		prepocheckerLayout.add(resultPanel);
 		
-		warning = new JLabel("");
-		warning.setForeground(Color.RED);
-		panel_2.add(warning);
+		result = new JLabel("");
+		result.setForeground(Color.RED);
+		resultPanel.add(result);
+		
+		/**
+		 * Adjust the result and warning
+		 */
 		btnSearch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if(textField.getText().isEmpty()){
-					warning.setText(EMPTY_SEARCH_STRING);
+					result.setText(EMPTY_SEARCH_STRING);
 				}
 				else{
 					//Controller controller = new Controller(textField.getText());
-					
+					String output = "default";
+					result.setText("RESULT: " + output);
 				}
 			}
 		});
 	}
-	
-	
 }
