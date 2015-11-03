96da97e175b28b4a13bb697ec3424fefb4b8c704
Le Cong Thang <lecongthang.1102@gmail.com>
2015-11-03 20:24:14 +0800

    no message

--- src/gui/ExecutionGUI.java
+++ src/gui/ExecutionGUI.java
@@ -1,41 +1,26 @@
 package gui;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
 import java.awt.BorderLayout;
-
-import javax.swing.JScrollPane;
-import javax.swing.border.EmptyBorder;
-import javax.swing.BoxLayout;
-
-import storage.CrawlerResult;
-
-import javax.swing.JSplitPane;
-import javax.swing.JLabel;
-import javax.swing.JTabbedPane;
-
-import java.awt.ScrollPane;
-import java.awt.Scrollbar;
-import java.awt.Checkbox;
-
-import javax.swing.JTextArea;
-
-import java.awt.FlowLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 
+import storage.CrawlerResult;
 import crawler.URL;
 
-import java.awt.Font;
-import java.awt.Color;
-import javax.swing.ScrollPaneConstants;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
+import javax.swing.border.LineBorder;
+import javax.swing.BoxLayout;
+import javax.swing.border.MatteBorder;
+import java.awt.FlowLayout;
 
 public class ExecutionGUI extends JFrame {
 	private final static String GUI_NAME = "EXECUTION GUI";
@@ -56,40 +41,53 @@ public class ExecutionGUI extends JFrame {
 	private void initialize() {
 		frame = new JFrame(GUI_NAME);
 		frame.setBounds(100, 100, 519, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
 		panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(255, 0, 0), 2));
 		panel.setLayout(new GridLayout(0, 2, 0, 0));
 		panel.setBackground(Color.WHITE);
 		panel.setForeground(Color.WHITE);
-		
-		JLabel lblNewLabel = new JLabel("URL");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Latency");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("");
-		panel.add(lblNewLabel_2);
-		
-		JLabel lblNewLabel_3 = new JLabel("");
-		panel.add(lblNewLabel_3);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
 		int v=ScrollPaneConstants. VERTICAL_SCROLLBAR_AS_NEEDED;
 		int h=ScrollPaneConstants. HORIZONTAL_SCROLLBAR_AS_NEEDED;
 		JScrollPane jsp = new JScrollPane(panel,v,h);
 		
+		JPanel panel_1 = new JPanel();
+		panel_1.setForeground(Color.WHITE);
+		panel_1.setBorder(new MatteBorder(0, 0, 0, 2, (Color) new Color(255, 0, 0)));
+		panel_1.setBackground(Color.WHITE);
+		panel.add(panel_1);
+		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+		
+		JLabel lblNewLabel = new JLabel("URL");
+		lblNewLabel.setForeground(new Color(0, 128, 0));
+		lblNewLabel.setVerticalAlignment(SwingConstants.TOP);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_1.add(lblNewLabel);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setForeground(Color.WHITE);
+		panel_2.setBorder(new MatteBorder(0, 0, 0, 0, (Color) new Color(255, 0, 0)));
+		panel_2.setBackground(Color.WHITE);
+		panel.add(panel_2);
+		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+		
+		JLabel lblNewLabel_1 = new JLabel("Latency");
+		lblNewLabel_1.setForeground(new Color(0, 128, 0));
+		lblNewLabel_1.setVerticalAlignment(SwingConstants.TOP);
+		panel_2.add(lblNewLabel_1);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		
 		jsp.setBorder(new EmptyBorder(20, 20, 20, 20));
 		frame.getContentPane().add(jsp);
 		frame.setVisible(true);
 		
 		for(int i = 0; i < 90; i++){
-			CrawlerResult c = new CrawlerResult(new URL("http://fsdfsvdfdfdssssssssssssssssssssssssssssssaf"), "html", 10.1);
+			CrawlerResult c = new CrawlerResult(new URL("http://fsdsdagfgjukdsgfdhfghtyuyttu67765675654353243fs"), "html", 10.1);
 			realTimeDisplay(c);
 		}
 	}
@@ -102,17 +100,30 @@ public class ExecutionGUI extends JFrame {
 		double latency = crawlerResult.latency;
 		String latencyText = Double.toString(latency);
 		
+		// URL
+		JPanel urlPanel = new JPanel();
+		urlPanel.setBorder(new MatteBorder(2, 0, 0, 2, (Color) new Color(255, 0, 0)));
+		urlPanel.setBackground(new Color(255, 255, 255));
+		urlPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+		
 		JLabel urlLabel = new JLabel(url);
-		urlLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		urlLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		urlLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(urlLabel);
+		urlLabel.setVerticalAlignment(SwingConstants.CENTER);
+		urlPanel.add(urlLabel);
+		panel.add(urlPanel);
+		
+		// Latency
+		JPanel latencyPanel = new JPanel();
+		latencyPanel.setBorder(new MatteBorder(2, 0, 0, 0, (Color) new Color(255, 0, 0)));
+		latencyPanel.setBackground(new Color(255, 255, 255));
+		latencyPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
 		
 		JLabel latencyLabel = new JLabel(latencyText);
-		latencyLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		latencyLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		latencyLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(latencyLabel);
-		
-		panel.add(urlLabel);
-		panel.add(latencyLabel);
+		latencyLabel.setVerticalAlignment(SwingConstants.CENTER);
+		latencyPanel.add(latencyLabel);
+		panel.add(latencyPanel);
 	}
 }
--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -2,27 +2,22 @@ package gui;
 
 import java.awt.Color;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
 import java.awt.Font;
-
-import javax.swing.JTextField;
-
-import java.awt.BorderLayout;
-
-import javax.swing.JButton;
-
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 import controller.Controller;
 import controller.QueryResult;
+import javax.swing.SwingConstants;
 
 public class GUI{
 	
@@ -35,10 +30,10 @@ public class GUI{
 	private JPanel panel;
 	private JPanel prepocheckerLayout;
 	private JPanel panel_1;
-	private JLabel result;
-	private JPanel resultPanel;
 	
 	Controller controller;
+	private JPanel resultPanel;
+	private JLabel result;
 
 	/**
 	 * Launch the application.
@@ -79,38 +74,50 @@ public class GUI{
 	private void initialize() {
 		frame = new JFrame(CRAWLER_NAME);
 		frame.setBounds(100, 100, 501, 330);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
 		prepocheckerLayout = new JPanel();
-		prepocheckerLayout.setBorder(new EmptyBorder(20, 20, 20, 20));
-		frame.getContentPane().add(prepocheckerLayout, BorderLayout.NORTH);
+		prepocheckerLayout.setBounds(0, 0, 485, 240);
+		prepocheckerLayout.setBorder(new EmptyBorder(20, 40, 20, 40));
+		frame.getContentPane().add(prepocheckerLayout);
 		prepocheckerLayout.setLayout(new BoxLayout(prepocheckerLayout, BoxLayout.Y_AXIS));
 		
 		panel = new JPanel();
 		prepocheckerLayout.add(panel);
-		panel.setBorder(new EmptyBorder(10, 10, 15, 10));
+		panel.setBorder(new EmptyBorder(30, 20, 20, 20));
 		
 		JLabel lblPrepochecker = new JLabel(CRAWLER_NAME);
+		lblPrepochecker.setToolTipText("Prepochecker");
 		panel.add(lblPrepochecker);
-		lblPrepochecker.setFont(new Font(".VnClarendonH", Font.PLAIN, 30));
+		lblPrepochecker.setFont(new Font(".VnClarendonH", Font.PLAIN, 35));
 		lblPrepochecker.setForeground(Color.BLUE);
 		
 		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		textField.setToolTipText("Search");
+		
+		// Create the border for textField
+		textField.setBorder(new LineBorder(new Color(60, 179, 113), 2));
 		prepocheckerLayout.add(textField);
 		
 		panel_1 = new JPanel();
-		panel_1.setBorder(new EmptyBorder(15, 10, 10, 10));
+		panel_1.setBorder(new EmptyBorder(15, 20, 20, 20));
 		prepocheckerLayout.add(panel_1);
 		
 		btnSearch = new JButton("Prepochecker Search");
+		btnSearch.setToolTipText("Search button");
+		btnSearch.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		panel_1.add(btnSearch);
 		
 		resultPanel = new JPanel();
+		resultPanel.setBorder(new EmptyBorder(0, 10, 10, 10));
 		prepocheckerLayout.add(resultPanel);
 		
-		result = new JLabel("");
-		result.setForeground(Color.RED);
+		result = new JLabel();
+		result.setToolTipText("Result");
+		result.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		result.setForeground(new Color(220, 20, 60));
 		resultPanel.add(result);
 		
 		/**
@@ -122,8 +129,8 @@ public class GUI{
 				if (input.isEmpty()) {
 					result.setText(EMPTY_SEARCH_STRING);
 				} else {
-					controller.query(input);
 					ExecutionGUI executionGUI = new ExecutionGUI();
+					controller.query(input);
 				}
 			}
 		});
